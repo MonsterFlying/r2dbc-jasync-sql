a517780aa9b48fe6e08784be3181f4033f1f278d
wangguo <wangguo>
2019-07-30 12:04:10 +0800

    spring-r2dbc

--- src/main/java/com/example/r2dbcjasyncsql/r2dbcjasyncsql/R2dbcJasyncSqlApplication.java
+++ src/main/java/com/example/r2dbcjasyncsql/r2dbcjasyncsql/R2dbcJasyncSqlApplication.java
@@ -1,31 +1,27 @@
 package com.example.r2dbcjasyncsql.r2dbcjasyncsql;
 
 import com.github.jasync.r2dbc.mysql.JasyncConnectionFactory;
+import com.github.jasync.r2dbc.mysql.MysqlConnectionFactoryProvider;
 import com.github.jasync.sql.db.mysql.pool.MySQLConnectionFactory;
 import io.r2dbc.spi.ConnectionFactory;
+import io.r2dbc.spi.ConnectionFactoryOptions;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.r2dbc.config.AbstractR2dbcConfiguration;
+import org.springframework.data.r2dbc.dialect.PostgresDialect;
 import org.springframework.data.r2dbc.function.DatabaseClient;
-import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
-import org.springframework.data.r2dbc.repository.query.Query;
+import org.springframework.data.r2dbc.function.DefaultReactiveDataAccessStrategy;
+import org.springframework.data.r2dbc.function.ReactiveDataAccessStrategy;
 import org.springframework.data.relational.core.mapping.Table;
-import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 
 
-interface CityRepository extends ReactiveCrudRepository<BasisCity, Long> {
-
-    @Query("SELECT * FROM basis_city")
-    Flux<BasisCity> list();
-
-}
 
 @SpringBootApplication
 public class R2dbcJasyncSqlApplication {
@@ -42,7 +38,6 @@ public class R2dbcJasyncSqlApplication {
 
 
         private final DatabaseClient connection;
-
         @GetMapping("list")
         public Flux<BasisCity> list() {
 
@@ -61,22 +56,24 @@ public class R2dbcJasyncSqlApplication {
     }
 
     @Configuration
-    @EnableR2dbcRepositories
-    public class JasynSqlDataSourceConfig extends AbstractR2dbcConfiguration {
+    public class JasynSqlDataSourceConfig {
         String url = "jdbc:mysql://10.2.10.59:3306/oem_cms?user=dfroot&password=H@slPZtn7eNKFP9bIEOso0I5nsz7YiZJ";
+        @Bean
+        public DatabaseClient connection() {
+            ConnectionFactoryOptions factoryOptions = ConnectionFactoryOptions.builder()
+                    .option(ConnectionFactoryOptions.HOST, "10.2.10.59")
+                    .option(ConnectionFactoryOptions.PASSWORD, "H@slPZtn7eNKFP9bIEOso0I5nsz7YiZJ")
+                    .option(ConnectionFactoryOptions.DATABASE, "oem_cms")
+                    .option(ConnectionFactoryOptions.PORT, 3306)
+                    .option(ConnectionFactoryOptions.USER, "dfroot")
+                    .build();
+            JasyncConnectionFactory jasyncConnectionFactory = new MysqlConnectionFactoryProvider().create(factoryOptions);
 
-        @Override
-        public ConnectionFactory connectionFactory() {
-            com.github.jasync.sql.db.Configuration configuration = new com.github.jasync.sql.db.Configuration(
-                    "dfroot",
-                    "10.2.10.59",
-                    3306,
-                    "H@slPZtn7eNKFP9bIEOso0I5nsz7YiZJ",
-                    "oem_cms"
-            );
-            MySQLConnectionFactory mySQLConnectionFactory = new MySQLConnectionFactory(configuration);
-            return new JasyncConnectionFactory(mySQLConnectionFactory);
+            return DatabaseClient.builder()
+                    .connectionFactory(jasyncConnectionFactory)
+                    .dataAccessStrategy(new DefaultReactiveDataAccessStrategy(PostgresDialect.INSTANCE)).build();
         }
+
     }
 
 
